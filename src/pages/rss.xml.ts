79f78a9c5bdf8a4ad7cbf7c808d2cd2bab9aa8ef
Tzu Yin <tzyn.wang@gmail.com>
2025-02-22 11:18:40 +0800

    [fix] use all capital

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 import { TITLE, DESCRIPTION, SITE } from '@Models/GeneralModels';
 import { RSS_POSTS } from '@Tools/post';
 
-export async function get() {
+export async function GET() {
   return rss({
     title: TITLE,
     description: DESCRIPTION,
