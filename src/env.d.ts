2f47a75b8712ca4e1f4ba572ba26c35c3ccae752
tzyn.wang <tzyn.wang@gmail.com>
2026-02-17 10:50:08 +0800

    [feat] add ts support

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
+/// <reference types="astro/client.d.ts" />
+
+// ref. https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript
+interface ImportMetaEnv {
+  readonly PUBLIC_GA4_ID: string;
+  readonly PUBLIC_GTAG_ID: string;
+  readonly PUBLIC_GISCUS_REPO_ID: string;
+  readonly PUBLIC_GISCUS_CATEGORY_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
